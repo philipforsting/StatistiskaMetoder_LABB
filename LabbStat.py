59e22c630cbb998e5c91d805cecab3c64d3355c0
Philip Forsting <Philip.Forsting@iths.se>
2026-02-10 15:48:53 +0100

    Hantering av NaN i fit(). Provade att göra en plot efter testanrop av fit()

--- LabbStat.py
+++ LabbStat.py
@@ -28,16 +28,18 @@ class LinjearRegression:
             X = X.reshape(-1, 1)
         elif X.ndim > 2:
             raise ValueError("X must be 1D or 2D")
+        elif np.isnan(X).any():
+            raise ValueError("X contains NaN errors?") # Är detta rimligt agerande? Kanske ska man ta bort rader med NaN? Oklart...
 
         if y.shape[0] != X.shape[0]:
             raise ValueError("X and y must have the same number of observations")
 
+        #Calculating output
+        self.X = np.column_stack([np.ones(n), X])
+        self.y = y        
         self._n = n = X.shape[0]
         self._d = d = X.shape[1]
-        self.X = np.column_stack([np.ones(n), X])
-        self.y = y
         self.beta_hat = np.linalg.pinv(self.X.T @ self.X) @self.X.T @ self.y
-
         return self
     
     def predict(self, X_new): #för att använda OLS
